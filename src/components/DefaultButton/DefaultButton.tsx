167e0c59c40b95b1145db6a8e2f27f718a731a9f
Ezequiel Mella <emleonardelli@gmail.com>
2021-07-28 19:11:30 -0300

    Test husky

--- src/components/DefaultButton/DefaultButton.tsx
+++ src/components/DefaultButton/DefaultButton.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
-import { Text, TouchableOpacity, ViewStyle } from 'react-native';
-import { styles } from './styles';
-    
+import {Text, TouchableOpacity, ViewStyle} from 'react-native';
+import {styles} from './styles';
+
 interface Props {
-    additionalStyle?: ViewStyle, 
-    texto: String,
-    onPress: () => void;
+  additionalStyle?: ViewStyle;
+  texto: String;
+  onPress: () => void;
 }
 
-export const DefaultButton = ({ onPress, texto, additionalStyle }: Props) => {
-    return (
-        <TouchableOpacity style={[styles.mainContainer, additionalStyle]} onPress={onPress}>
-            <Text style={styles.text}>{texto}</Text>
-        </TouchableOpacity>
-    );
-}
+export const DefaultButton = ({onPress, texto, additionalStyle}: Props) => {
+  return (
+    <TouchableOpacity
+      style={[styles.mainContainer, additionalStyle]}
+      onPress={onPress}>
+      <Text style={styles.text}>{texto}</Text>
+    </TouchableOpacity>
+  );
+};
 
 DefaultButton.defaultProps = {
-    additionalStyle: {},
+  additionalStyle: {},
 };
